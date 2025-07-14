4d9b1bcbffef639cf288abde94c8b3b3fe087cda
erenggs <erenggs21@gmail.com>
2025-07-15 04:26:55 +0600

    Update bby.js

--- scripts/cmds/bby.js
+++ scripts/cmds/bby.js
@@ -5,7 +5,7 @@ const baseApiUrl = async () => {
 
 module.exports.config = {
     name: "bby",
-    aliases: ["baby", "bbe", "babe", "sam", "bot", "chat"],
+    aliases: ["baby", "hlw", "hi", "eren", "mikasa", "chat"],
     version: "6.9.0",
     author: "dipto | nexo_here",
     countDown: 0,
@@ -149,7 +149,7 @@ module.exports.onChat = async ({ api, event, message }) => {
         const body = event.body ? event.body.toLowerCase() : "";
         if (body.startsWith("baby") || body.startsWith("bby") || body.startsWith("bot") || body.startsWith("jan") || body.startsWith("babu") || body.startsWith("janu")) {
             const arr = body.replace(/^\S+\s*/, "");
-            const randomReplies = ["Bol 🐸", "Bol suntechi 🐍", "Dakis ken 😾?", "Bolo jaan ki korte pari tomar jonno 😞"];
+            const randomReplies = ["Bol 🐸", "Bol suntechi 🐍", "Dakis ken 😾?", "Bolo ki korte pari tomar jonno 😞"];
             if (!arr) {
                 await api.sendMessage(randomReplies[Math.floor(Math.random() * randomReplies.length)], event.threadID, (error, info) => {
                     if (!info) message.reply("info obj not found");
