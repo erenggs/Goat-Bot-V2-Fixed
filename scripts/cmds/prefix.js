4ea0c3f868ed481ac07370372f11ca16c938572d
eren999x <Ffan4684@gmail.com>
2025-07-25 12:30:18 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "Yo senpai 🐔\n\nMy prefix is: %1\nYor group chat prefix is: %2"
+			myPrefix: "hello senpai did you call me?\n\nMy prefix is: %1\nYor group chat prefix is: %2"
 		}
 	},
 
