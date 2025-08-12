37fc8c1e28b0505374ded3a288e902bea02f0162
eren999x <Ffan4684@gmail.com>
2025-08-12 23:48:36 +0600

    Update Screenshot.js

--- fb-chat-api/src/Screenshot.js
+++ fb-chat-api/src/Screenshot.js
@@ -1,83 +1,88 @@
 /* eslint-disable linebreak-style */
 "use strict";
 
-
 module.exports = function (defaultFuncs, api, ctx) {
-    var Coookie = JSON.parse(JSON.stringify(ctx.jar.getCookies("https://www.facebook.com").concat(ctx.jar.getCookies("https://facebook.com")).concat(ctx.jar.getCookies("https://www.messenger.com"))));
-    for (let i of Coookie) {
-        i.name = i.key;
-        i.domain = 'www.facebook.com';
-        delete i.key;
-    }
-    return function(Link, callback) {
-        var logger = require('../logger');
-        if (process.platform != 'win32') return logger.Error('Not Supported Platform');
-        else try {
-            let i = require('puppeteer');
-        }   
-        catch (e) {
-            var { execSync } = require('child_process');
-            execSync('npm i puppeteer', { stdio: 'inherit' });
-        }
-            const Screenshot = require('../Extra/ExtraScreenShot');
-                var resolveFunc = function () { };
-                var rejectFunc = function () { };
-                var returnPromise = new Promise(function (resolve, reject) {
-                resolveFunc = resolve;
-                rejectFunc = reject;
-                });
+    // Collect cookies from all Facebook-related domains
+    let Cookies = [
+        ...ctx.jar.getCookies("https://www.facebook.com"),
+        ...ctx.jar.getCookies("https://facebook.com"),
+        ...ctx.jar.getCookies("https://www.messenger.com")
+    ].map(cookie => ({
+        ...cookie,
+        name: cookie.key,
+        domain: "www.facebook.com",
+        ...(delete cookie.key && cookie)
+    }));
 
-                if (!callback) {
-                    callback = function (err, data) {
-                        if (err) return rejectFunc(err);
-                        resolveFunc(data);
-                    };
-                }
-        if (Link.includes('facebook.com') || Link.includes('Facebook.com') || Link.includes('fb')) {
-            let LinkSplit = Link.split('/');
-                if (LinkSplit.indexOf("https:") == 0) {
-                    if (Link.includes('messages')) {
-                        Screenshot.buffer(Link, {
-                            cookies: Coookie
-                        }).then(data => {
-                            callback(null,data);
-                        });
-                    }
-                    else if (!isNaN(LinkSplit[3]) && !Link.split('=')[1]  && !isNaN(Link.split('=')[1])) {
-                        api.sendMessage('Invaild link, format link: facebook.com/Lazic.Kanzu',global.Fca.Data.event.threadID,global.Fca.Data.event.messageID);
-                        callback('Error Link', null);
-                    }
-                    else if (!isNaN(Link.split('=')[1]) && Link.split('=')[1]) {
-                        let Format = `https://www.facebook.com/profile.php?id=${Link.split('=')[1]}`;
-                        Screenshot.buffer(Format, {
-                            cookies: Coookie
-                        }).then(data => {
-                            callback(null,data);
-                        });
-                    } 
-                    else {
-                        let Format = `https://www.facebook.com/${LinkSplit[3]}`;
-                        Screenshot.buffer(Format, {
-                            cookies: Coookie
-                        }).then(data => {
-                            callback(null,data);
-                        });
-                    }
-                }
-                else {
-                    let Form = `https://www.facebook.com/${LinkSplit[1]}`;
-                    Screenshot.buffer(Form, {
-                        cookies: Coookie
-                    }).then(data => {
-                        callback(null,data);
-                    });
-                }
+    return function (Link, callback) {
+        const logger = require("../logger");
+
+        // Ensure Puppeteer is installed
+        try {
+            require("puppeteer");
+        } catch {
+            const { execSync } = require("child_process");
+            try {
+                execSync("npm install puppeteer", { stdio: "inherit" });
+                logger.Info("Puppeteer installed successfully.");
+            } catch (installErr) {
+                logger.Error("Failed to install Puppeteer:", installErr);
+                return callback(installErr, null);
             }
-                else {
-                    Screenshot.buffer(Link).then(data => {
-                        callback(null,data);
-                    });
+        }
+
+        const Screenshot = require("../Extra/ExtraScreenShot");
+
+        let resolveFunc, rejectFunc;
+        const returnPromise = new Promise((resolve, reject) => {
+            resolveFunc = resolve;
+            rejectFunc = reject;
+        });
+
+        if (!callback) {
+            callback = (err, data) => {
+                if (err) return rejectFunc(err);
+                resolveFunc(data);
+            };
+        }
+
+        (async () => {
+            try {
+                // Normalize link for screenshot capture
+                let finalLink = Link;
+
+                if (/facebook\.com|fb/i.test(Link)) {
+                    const parts = Link.split("/");
+
+                    if (Link.startsWith("https:")) {
+                        if (Link.includes("messages")) {
+                            finalLink = Link; // Messenger chat link
+                        } else {
+                            // Check for numeric ID
+                            const idMatch = Link.match(/id=(\d+)/);
+                            if (idMatch) {
+                                finalLink = `https://www.facebook.com/profile.php?id=${idMatch[1]}`;
+                            } else if (parts[3]) {
+                                finalLink = `https://www.facebook.com/${parts[3]}`;
+                            }
+                        }
+                    } else {
+                        // Short format facebook.com/username
+                        finalLink = `https://www.facebook.com/${parts[1]}`;
+                    }
+
+                    return callback(null, await Screenshot.buffer(finalLink, { cookies: Cookies }));
                 }
-            return returnPromise;
+
+                // For non-Facebook links
+                return callback(null, await Screenshot.buffer(Link));
+
+            } catch (err) {
+                logger.Error("Screenshot capture failed:", err);
+                callback(err, null);
+            }
+        })();
+
+        return returnPromise;
     };
 };
