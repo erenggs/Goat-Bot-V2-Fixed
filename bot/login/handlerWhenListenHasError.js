84a053128d5dadd991d74a708d093209dda3ab73
eren999x <Ffan4684@gmail.com>
2025-08-15 18:34:29 +0600

    Update handlerWhenListenHasError.js

--- bot/login/handlerWhenListenHasError.js
+++ bot/login/handlerWhenListenHasError.js
@@ -1,110 +1,118 @@
 const axios = require('axios');
-function filterAddress(address) {
-	return address.split(/[,;\s]/).map(id => id.trim()).filter(id => id);
-}
-// this is handler will run when listen has error (api.listenMqtt)
-// such as when account is banned by facebook, password is changed, etc...
-module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, error }) {
-	const { config, botID } = global.GoatBot;
-	const { log } = global.utils;
-	const configNotiWhenListenMqttError = config.notiWhenListenMqttError || {};
-	// YOUR CODE HERE
 
-	/* ___ Example send a MAIL to admin when bot has error ___ */
-	if (configNotiWhenListenMqttError.gmail?.enable == true) {
-		const { utils } = global;
+function filterAddress(address) {
+	return address
+		.split(/[,;\s]/)
+		.map(id => id.trim())
+		.filter(id => id);
+}
+
+// This handler runs when api.listenMqtt encounters an error
+// (e.g., account banned, password changed, etc.)
+module.exports = async function ({
+	api,
+	threadModel,
+	userModel,
+	dashBoardModel,
+	globalModel,
+	threadsData,
+	usersData,
+	dashBoardData,
+	globalData,
+	error
+}) {
+	const { config, botID } = global.GoatBot;
+	const { log, utils } = global;
+
+	const configNoti = config.notiWhenListenMqttError || {};
+
+	/* ===== SEND EMAIL TO ADMIN ===== */
+	if (configNoti.gmail?.enable) {
 		const { sendMail, Prism } = utils;
 		let highlightCode = error;
-		if (typeof error == "object" && !error.stack)
+
+		if (typeof error === "object" && !error.stack)
 			highlightCode = Prism.highlight(JSON.stringify(error, null, 2), Prism.languages.json, 'json');
 		else if (error.stack)
 			highlightCode = Prism.highlight(error.stack, Prism.languages.jsstacktrace, 'jsstacktrace');
 
-		const mailAddress = filterAddress(configNotiWhenListenMqttError.gmail.emailGetNoti);
-		for (const mail of mailAddress) {
-			if (!mail)
-				continue;
+		const mailAddresses = filterAddress(configNoti.gmail.emailGetNoti);
+		for (const mail of mailAddresses) {
+			if (!mail) continue;
+
 			sendMail({
 				to: mail,
-				subject: "Report error when listen message in Goat Bot",
+				subject: `Goat Bot Error Report (Bot ID: ${botID})`,
 				text: "",
-				html: `<h2>Has error when listen message in Goat Bot id: ${botID}</h2><div><pre style="background:#272822;position: relative;padding: 1em 0 1em 1em;"><code style="color:#272822;background:#272822;text-shadow:0 1px rgba(0,0,0,.3);font-family:Consolas,Monaco,'Andale Mono','Ubuntu Mono',monospace;font-size:1em;text-align:left;">${highlightCode}</code></pre></div>`
+				html: `<h2>Error in Goat Bot (ID: ${botID})</h2>
+				<div><pre style="background:#272822;padding:1em;"><code style="color:#f8f8f2;font-family:Consolas,Monaco,monospace;">${highlightCode}</code></pre></div>`
 			})
-				.then(data => {
-					// CUSTOM YOUR CODE HERE
-				})
-				.catch(err => log.err("handlerWhenListenHasError", "Can not send mail to admin", err));
+				.then(() => log.info("handlerWhenListenHasError", `Mail sent to ${mail}`))
+				.catch(err => log.err("handlerWhenListenHasError", "Cannot send mail", err));
 		}
 	}
 
-	/* ___ Example send a message to TELEGRAM when bot has error ___ */
-	if (configNotiWhenListenMqttError.telegram?.enable == true) {
-		const TELEBOT_TOKEN = configNotiWhenListenMqttError.telegram.botToken;
+	/* ===== SEND TELEGRAM MESSAGE TO ADMIN ===== */
+	if (configNoti.telegram?.enable) {
+		const TELEBOT_TOKEN = configNoti.telegram.botToken;
 		let highlightCode = error;
-		if (typeof error == "object" && !error.stack)
+
+		if (typeof error === "object" && !error.stack)
 			highlightCode = JSON.stringify(error, null, 2);
 		else if (error.stack)
 			highlightCode = error.stack;
 
-		const ADMIN_IDS_TELEGRAM = filterAddress(configNotiWhenListenMqttError.telegram.chatId);
-		for (const ADMIN_ID_TELEGRAM of ADMIN_IDS_TELEGRAM) {
-			if (!ADMIN_ID_TELEGRAM)
-				continue;
-			const MAX_LENGTH_TELEGRAM_MESSAGE = 4096; // 4096 is max length of message in telegram
-			const message = `Has error when listen message in Goat Bot id: ${botID}:\n`;
-			let messageError = `\`\`\`json\n${highlightCode}\n\`\`\``;
+		const ADMIN_IDS = filterAddress(configNoti.telegram.chatId);
+		for (const ADMIN_ID of ADMIN_IDS) {
+			if (!ADMIN_ID) continue;
 
-			if (message.length + messageError.length > MAX_LENGTH_TELEGRAM_MESSAGE) {
+			const MAX_LENGTH = 4096;
+			const messageHeader = `Error in Goat Bot (ID: ${botID}):\n`;
+			let messageBody = `\`\`\`json\n${highlightCode}\n\`\`\``;
+
+			if ((messageHeader.length + messageBody.length) > MAX_LENGTH) {
 				const lastString = "\n\n... (Too long to show)```";
-				messageError = messageError.slice(0, MAX_LENGTH_TELEGRAM_MESSAGE - message.length - lastString.length) + lastString;
+				messageBody = messageBody.slice(0, MAX_LENGTH - messageHeader.length - lastString.length) + lastString;
 			}
 
 			axios.post(`https://api.telegram.org/bot${TELEBOT_TOKEN}/sendMessage`, {
-				chat_id: ADMIN_ID_TELEGRAM,
-				text: message + messageError,
+				chat_id: ADMIN_ID,
+				text: messageHeader + messageBody,
 				parse_mode: "Markdown"
 			})
-				.then(data => {
-					// CUSTOM YOUR CODE HERE
-				})
-				.catch(err => log.err("handlerWhenListenHasError", "Can not send message to telegram", err.response?.data));
+				.then(() => log.info("handlerWhenListenHasError", `Telegram message sent to ${ADMIN_ID}`))
+				.catch(err => log.err("handlerWhenListenHasError", "Cannot send Telegram message", err.response?.data));
 		}
 	}
 
-	/* ___ Example send a message to WEBHOOK DISCORD when bot has error ___ */
-	if (configNotiWhenListenMqttError.discordHook?.enable == true) {
+	/* ===== SEND DISCORD WEBHOOK MESSAGE ===== */
+	if (configNoti.discordHook?.enable) {
 		let highlightCode = error;
-		const content = `**Has error when listen message in Goat Bot id: ${botID}:**\n\`\`\`json\n{highlightCode}\n\`\`\``;
-		const contentLength = content.replace("{highlightCode}").length;
-		if (typeof error == "object" && !error.stack)
+
+		if (typeof error === "object" && !error.stack)
 			highlightCode = JSON.stringify(error, null, 2);
 		else if (error.stack)
 			highlightCode = error.stack;
 
-		const MAX_LENGTH_DISCORD_MESSAGE = 2000; // 2000 is max length of message in discord webhook
-		if (highlightCode.length + contentLength > MAX_LENGTH_DISCORD_MESSAGE) {
+		const baseContent = `**Error in Goat Bot (ID: ${botID}):**\n\`\`\`json\n{highlightCode}\n\`\`\``;
+		const MAX_LENGTH = 2000;
+
+		let content = baseContent.replace("{highlightCode}", highlightCode);
+		if (content.length > MAX_LENGTH) {
 			const lastString = "\n\n... (Too long to show)```";
-			highlightCode = highlightCode.slice(0, MAX_LENGTH_DISCORD_MESSAGE - contentLength - lastString.length) + lastString;
+			highlightCode = highlightCode.slice(0, MAX_LENGTH - baseContent.length - lastString.length) + lastString;
+			content = baseContent.replace("{highlightCode}", highlightCode);
 		}
 
-		const jsonHook = {
-			content: content.replace("{highlightCode}", highlightCode),
-			embeds: null,
-			attachments: []
-		};
-
-		const webhookUrls = filterAddress(configNotiWhenListenMqttError.discordHook.webhookUrl);
+		const webhookUrls = filterAddress(configNoti.discordHook.webhookUrl);
 		for (const WEBHOOK of webhookUrls) {
-			if (!WEBHOOK)
-				continue;
-			axios.post(WEBHOOK, jsonHook)
-				.then(data => {
-					// CUSTOM YOUR CODE HERE
-				})
-				.catch(err => log.err("handlerWhenListenHasError", "Can not send message to discord webhook", err.response?.data));
+			if (!WEBHOOK) continue;
+
+			axios.post(WEBHOOK, { content, embeds: null, attachments: [] })
+				.then(() => log.info("handlerWhenListenHasError", `Discord webhook sent to ${WEBHOOK}`))
+				.catch(err => log.err("handlerWhenListenHasError", "Cannot send Discord webhook", err.response?.data));
 		}
 	}
 
-	/* AND YOU CAN CUSTOM YOUR CODE HERE */
-
+	/* ===== CUSTOM HANDLER CODE CAN GO HERE ===== */
 };
