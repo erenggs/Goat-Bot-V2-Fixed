d43b6e2e3e26fa07d31fbac199808b891b5660b2
eren999x <Ffan4684@gmail.com>
2025-08-15 18:42:49 +0600

    Update socketIO.js

--- bot/login/socketIO.js
+++ bot/login/socketIO.js
@@ -1,45 +1,58 @@
-// This module will be called if enabled in the config (severUptime.socket.enable = true)
 /**
- * @example for connect to socket.io
- * view file ./connectSocketIO.example.js
+ * Socket.IO server module for GoatBot uptime monitoring
+ * This module will be called if enabled in config (serverUptime.socket.enable = true)
+ * @example See ./connectSocketIO.example.js
  */
 const { Server } = require("socket.io");
 const { log, getText } = global.utils;
 const { config } = global.GoatBot;
 
 module.exports = async (server) => {
-	const { channelName, verifyToken } = config.serverUptime.socket;
-	let io;
+    const { channelName, verifyToken } = config.serverUptime.socket || {};
+    let io;
 
-	try {
-		if (!channelName)
-			throw ('"channelName" is not defined in config');
-		if (!verifyToken)
-			throw ('"verifyToken" is not defined in config');
-		io = new Server(server);
-		log.info("SOCKET IO", getText("socketIO", "connected"));
-	}
-	catch (err) {
-		return log.err("SOCKET IO", getText("socketIO", "error"), err);
-	}
+    // Validate config
+    if (!channelName) {
+        return log.err("SOCKET IO", getText("socketIO", "error"), '"channelName" is not defined in config');
+    }
+    if (!verifyToken) {
+        return log.err("SOCKET IO", getText("socketIO", "error"), '"verifyToken" is not defined in config');
+    }
 
-	io.on("connection", (socket) => {
-		if (socket.handshake.query.verifyToken != verifyToken) {
-			io.to(socket.id).emit(channelName, {
-				status: "error",
-				message: "Token is invalid"
-			});
-			socket.disconnect();
-			return;
-		}
-		log.info("SOCKET IO", `New client connected to socket: ${socket.id}`);
-		io.to(socket.id).emit(channelName, {
-			status: "success",
-			message: "Connected to server successfully"
-		});
-		socket.on("disconnect", () => {
-			log.info("SOCKET IO", `Client disconnected from socket: ${socket.id}`);
-		});
-	});
+    try {
+        io = new Server(server);
+        log.info("SOCKET IO", getText("socketIO", "connected"));
+    } catch (err) {
+        return log.err("SOCKET IO", getText("socketIO", "error"), err);
+    }
+
+    io.on("connection", (socket) => {
+        const token = socket.handshake.query?.verifyToken;
+
+        // Validate token
+        if (token !== verifyToken) {
+            socket.emit(channelName, {
+                status: "error",
+                message: "Token is invalid"
+            });
+            socket.disconnect(true);
+            log.warn("SOCKET IO", `Client ${socket.id} provided invalid token`);
+            return;
+        }
+
+        log.info("SOCKET IO", `New client connected: ${socket.id}`);
+        socket.emit(channelName, {
+            status: "success",
+            message: "Connected to server successfully"
+        });
+
+        socket.on("disconnect", (reason) => {
+            log.info("SOCKET IO", `Client disconnected: ${socket.id} | Reason: ${reason}`);
+        });
+    });
+
+    // Optional: handle server-side errors
+    io.on("error", (err) => {
+        log.err("SOCKET IO", "Socket.IO server error", err);
+    });
 };
-
