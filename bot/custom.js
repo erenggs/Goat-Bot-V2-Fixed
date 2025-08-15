6435b370538f361df978fac44a7ad65d37c1a4d9
eren999x <Ffan4684@gmail.com>
2025-08-15 18:21:35 +0600

    Update custom.js

--- bot/custom.js
+++ bot/custom.js
@@ -1,16 +1,36 @@
 const { log } = global.utils;
 
-module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, getText }) {
-	// This is where you can add your custom code to the bot.
-	// The bot will run this code every time it starts up (after logging in and loading data from the database).
+module.exports = async function ({
+	api,
+	threadModel,
+	userModel,
+	dashBoardModel,
+	globalModel,
+	threadsData,
+	usersData,
+	dashBoardData,
+	globalData,
+	getText
+}) {
+	// This runs once every time the bot starts up (after login and DB load)
 
-	setInterval(async () => {
-		api.refreshFb_dtsg()
-			.then(() => {
-				log.succes("refreshFb_dtsg", getText("custom", "refreshedFb_dtsg"));
-			})
-			.catch((err) => {
-				log.error("refreshFb_dtsg", getText("custom", "refreshedFb_dtsgError"), err);
-			});
-	}, 1000 * 60 * 60 * 48); // 48h
+	// Function to refresh fb_dtsg token
+	const refreshToken = async () => {
+		if (typeof api.refreshFb_dtsg !== 'function') {
+			return log.error('refreshFb_dtsg', 'api.refreshFb_dtsg is not available');
+		}
+
+		try {
+			await api.refreshFb_dtsg();
+			log.success('refreshFb_dtsg', getText('custom', 'refreshedFb_dtsg'));
+		} catch (err) {
+			log.error('refreshFb_dtsg', getText('custom', 'refreshedFb_dtsgError'), err.message || err);
+		}
+	};
+
+	// Run every 48 hours (1000ms * 60s * 60m * 48h)
+	setInterval(refreshToken, 1000 * 60 * 60 * 48);
+
+	// Optionally, run once immediately on startup
+	refreshToken();
 };
